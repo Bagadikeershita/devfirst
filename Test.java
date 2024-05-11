e6042b08021c3f0bd90238af68c7199e19734e43
Bagadikeershita <143019501+Bagadikeershita@users.noreply.github.com>
2024-05-11 10:24:03 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,7 +1,6 @@
 public class Test{
   public static void main(String args[])
   {
-  System.out.println("Hiiii");
-  System.out.println("I am Keershita");
-  System.out.println("Today is devops lab external "); 
+  System.out.println("Hello");
+  System.out.println("This is Indra Reddy");
 }}
